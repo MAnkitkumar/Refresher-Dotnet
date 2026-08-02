eec9ee1e6e1f545a4aab763f56280452856d1435
Ankit Kumar <ankitkumar@example.com>
2026-08-02 20:38:34 +0530

    first commit

---
+++ Problems/Problem2.cs
@@ -0,0 +1,52 @@
+using System;
+namespace BankSys
+{
+    public class Account
+    {
+        // TODO: Add private fields
+        private string name;
+        private double balance;
+
+        // TODO: Implement constructor
+        public Account(string name, double initialBalance)
+        {
+            this.name = name;
+            this.balance = initialBalance;
+        }
+
+        // TODO: Implement deposit method
+        public double deposit(double depositAmount)
+        {
+            balance += depositAmount;
+            return balance;
+        }
+
+        // TODO: Implement getBalance method
+        public double getBalance()
+        {
+            return balance;
+        }
+
+        // TODO: Implement setName method
+        public void setName(string newName)
+        {
+            name = newName;
+        }
+
+        // TODO: Implement getName method
+        public string getName()
+        {
+            return name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test your implementation here
+            Account account1 = new Account("Alok Mittal", 1250.00);
+            Console.WriteLine(account1.getBalance());
+        }
+    }
+}
